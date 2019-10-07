79a3236e6fb07ad3547a5fc36753c68a25106a59
Wildtyto <wildtyto@gmail.com>
2019-10-07 14:53:13 +0800

    [ Changed ] - Update content

--- src/chapter/About.js
+++ src/chapter/About.js
@@ -22,15 +22,18 @@ export default class About extends React.Component {
                                 <span style={ { bottom: "60%", left: "70%" } }>React Native</span>
                                 <span style={ { bottom: "70%", left: "30%" } }>AngularJS</span>
                                 <span style={ { bottom: "70%", left: "60%" } }>Vue.js</span>
-                                <span style={ { bottom: "40%", left: "40%" } }>Vue Server Renderer</span>
-                                <span style={ { bottom: "40%", left: "80%" } }>Webpack</span>
+                                <span style={ { bottom: "40%", left: "50%" } }>Vue Server Renderer</span>
+                                <span style={ { bottom: "40%", left: "10%" } }>SASS</span>
+                                <span style={ { bottom: "40%", left: "90%" } }>Webpack</span>
                                 <span style={ { bottom: "50%", left: "80%" } }>NPM</span>
                                 <span style={ { bottom: "50%", left: "30%" } }>Google Analytics</span>
                                 <span style={ { bottom: "60%", left: "40%" } }>Express</span>
+                                <span style={ { bottom: "60%", left: "20%" } }>Scrum</span>
                                 <span style={ { bottom: "80%", left: "30%" } }>LINE Bot SDK</span>
                                 <span style={ { bottom: "50%", left: "60%" } }>PM2</span>
                                 <span style={ { bottom: "80%", left: "90%" } }>RESTful API</span>
                                 <span style={ { bottom: "30%", left: "50%" } }>Axios</span>
+                                <span style={ { bottom: "30%", left: "70%" } }>SVG</span>
                                 <span style={ { bottom: "30%", left: "30%" } }>Request</span>
                                 <span style={ { bottom: "20%", left: "40%" } }>SQLite</span>
                                 <span style={ { bottom: "90%", left: "30%" } }>HTML5</span>
@@ -45,9 +48,9 @@ export default class About extends React.Component {
                         </div>
                     </div>
                     <div className="about-bio">
-                        <p className="about-block">Since the first time I created my own website when I was 10 years old. I took a fancy to do creation of all about digital content. Such as animation, game and software. It defined my choice of my career in the future. Most of time, I was able to learn new skills of programming. However, I was concerned about the design and the thinking of the users. The point was the key I decided to major in media design. Because of that, I am good at both of web application development and user experience.</p>
-                        <p className="about-block">Recently, my colleague and I designed a service in a hackathon. After that, we rebuilt the idea to a new chat bot -- noper. It helped people who usually have business conversation at messaging tools when they were working. To manage and notify progress of the tasks with team members. We released the service for public for few monthes. Although there were many users registered our service, it was hard to be main income for us.</p>
-                        <p className="about-block">This years, I did full time jobs of front end developer and freelace jobs of full stack developer for few times. That is interesting to have chance to create different kind of products. I like to work with people cooperatively. Also I am excited to face challenge and solve them. With my skills of front end development and knowledge of back end. Hoping to have the opportunity to make more great works with great people. </p>
+                        <p className="about-block">Since the first time I created my own website when I was 10 years old. I took a fancy to do creation of all about digital content. Such as animation, game and tool software. It defined my choice of my career in the future. Most of time, I was able to learn new skills of programming by my self. However, I was concerned about the design and the thinking of the users. The point was the key I decided to major in media design. Because of that, I am good at both of development and user experience.</p>
+                        <p className="about-block">Recently, my colleague and I designed a service in a hackathon. After that, we rebuilt the idea to a new chat bot -- noper. It helped people who usually have business conversation at messaging tools when they were working. To manage and notify progress of the tasks with team members. We released the service for public for few monthes. Although there were many users registered our service, it was hard to be main income for us. Unfortunately, we decided to close the project.</p>
+                        <p className="about-block">This years, I did full time jobs of front end developer and freelace jobs of full stack developer for few times. That is interesting to have chance to create different kind of products. I like to work with people cooperatively. Also I am excited to face challenge and solve them. Additionally, it is interesting to research new technology to me. With my skills of front end development and knowledge of back end. Hoping to have the opportunity to make more great works with great people. </p>
                     </div>
                 </section>
             );
